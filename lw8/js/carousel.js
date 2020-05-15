3e04bc1d19212904770ab72e51505e2c25c73199
IrinaRogacheva <61589085+IrinaRogacheva@users.noreply.github.com>
2020-05-15 16:45:20 +0300

    lw8

--- lw8/js/carousel.js
+++ lw8/js/carousel.js
@@ -1,39 +1,31 @@
-carousel();
-
 function carousel() {
     const leftArrow = document.getElementById('left_arrow');
     const rightArrow = document.getElementById('right_arrow');
     const films = document.querySelectorAll('.film_block');
     const listOfFilms = document.querySelector('.carousel');
-    const numberOfFilms = films.length - 1;
-    const mainFilms = 3;
+    let numberOfFilms = films.length - 1;
     let i = 0;
-    let count = mainFilms;
 
-    rightArrow.onclick = function(){
-        if (count <= films.length + 1) {
-            listOfFilms.appendChild(films[i]);
-            count++;
+    function slideRight(){
+        listOfFilms.appendChild(films[i]);
+        if (i < numberOfFilms) {
             i++;
         } else {
-            listOfFilms.appendChild(films[i]);
-            count++;
-            if (count == films.length + mainFilms) {
-                count = mainFilms;
-                i = 0;
-            }
+            i = 0;
         }
     }
 
-    leftArrow.onclick = function(){
-        if (count > mainFilms) {
-            listOfFilms.insertBefore(films[i - 1], films[i]);
-            i--;
-            count--;
-        } else {
-            listOfFilms.insertBefore(films[numberOfFilms], films[i]);
+    function slideLeft(){
+        if (i <= 0) {
             i = numberOfFilms;
-            count = films.length + mainFilms - 1;
+            listOfFilms.insertBefore(films[numberOfFilms], films[numberOfFilms - i]);
+        } else {
+            i--;
+            listOfFilms.insertBefore(films[i], films[i + 1]);
         }
     }
+    rightArrow.addEventListener("click", slideRight);
+    leftArrow.addEventListener("click", slideLeft);
 }
+
+window.onload = carousel;
